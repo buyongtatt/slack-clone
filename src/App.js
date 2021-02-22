0aa2bbe1b6353380e63f306f293e98e6fbc37119
021120 <yongtatt981213@hotmail.com>
2021-02-22 17:43:54 +0800

    Header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Switch>
-          <Route path="/"></Route>
+          <Route path="/">
+            <Header />
+          </Route>
         </Switch>
       </Router>
     </div>
